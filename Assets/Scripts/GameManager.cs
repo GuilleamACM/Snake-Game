14f55fa671a5cb45b4e16deee336840ba35a3f69
GuilleamACM <gacm@cin.ufpe.br>
2020-03-13 16:03:34 -0300

    Map data and player gameobject created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    [Header("Player Settigns")]
+    public Color playerColor = Color.black;
+    GameObject playerGameObject;
+
     [Header("Map Settings")]
     public int mapHeight = 15;
     public int mapWidth = 17;
@@ -13,22 +17,37 @@ public class GameManager : MonoBehaviour
 
     GameObject mapGameObject;
     SpriteRenderer mapRenderer;
+    Node[,] mapGrid;
+
     // Start is called before the first frame update
     void Start()
     {
         CreateMap();
+        PlacePlayer();
     }
 
     void CreateMap()
     {
         mapGameObject = new GameObject("Map");
         mapRenderer = mapGameObject.AddComponent<SpriteRenderer>();
-
+        mapGrid = new Node[mapWidth, mapHeight];
         Texture2D mapTexture = new Texture2D(mapWidth, mapHeight);
+
         for (int x = 0; x < mapWidth; x++)
         {
             for (int y = 0; y < mapHeight; y++)
             {
+                Vector3 vector = Vector3.zero;
+                vector.x = x;
+                vector.y = y;
+                Node node = new Node()
+                {
+                    x = x,
+                    y = y,
+                    worldPosition = vector
+                };
+                mapGrid[x, y] = node;
+
                 #region Map Visual
                 if (x % 2 != 0)
                 {
@@ -60,5 +79,33 @@ public class GameManager : MonoBehaviour
         Rect rect = new Rect(0, 0, mapWidth, mapHeight);
         Sprite sprite = Sprite.Create(mapTexture, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
         mapRenderer.sprite = sprite;
+    }
+
+    Sprite CreateSprite(Color targerColor)
+    {
+        Texture2D texture = new Texture2D(1, 1);
+        texture.filterMode = FilterMode.Point;
+        texture.SetPixel(0, 0, targerColor);
+        texture.Apply();
+        Rect rect = new Rect(0, 0, 1, 1);
+        return Sprite.Create(texture, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
+    }
+
+    void PlacePlayer()
+    {
+        playerGameObject = new GameObject("Player");
+        SpriteRenderer playerRender = playerGameObject.AddComponent<SpriteRenderer>();
+        playerRender.sprite = CreateSprite(playerColor);
+        playerRender.sortingOrder = 1;
+        playerGameObject.transform.position = GetNode(3, 3).worldPosition;
+    }
+
+    Node GetNode(int x, int y)
+    {
+        if(x < 0 || x > mapWidth - 1 || y < 0 || y > mapHeight - 1)
+        {
+            return null;
+        }
+        return mapGrid[x, y];
     }
 }
