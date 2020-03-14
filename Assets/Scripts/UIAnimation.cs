dd3e1718fd7433b0176104689768caec20b20dce
GuilleamACM <gacm@cin.ufpe.br>
2020-03-14 16:44:06 -0300

    Fade animations and post-processing added to the UI

---
+++ Assets/Scripts/UIAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIAnimation : MonoBehaviour
+{
+    public Text startText;
+    public Text restartText;
+    Sequence startSequence;
+    Sequence restartSequence;
+    void Awake()
+    {
+        startSequence = DOTween.Sequence();
+        startSequence.Append(startText.DOFade(0f, 1f));
+        startSequence.Append(startText.DOFade(1f, 1f));
+        startSequence.SetLoops(-1);
+        restartSequence = DOTween.Sequence();
+        restartSequence.Append(restartText.DOFade(0f, 1f));
+        restartSequence.Append(restartText.DOFade(1f, 1f));
+        restartSequence.SetLoops(-1);
+    }
+
+    public void PlayStartSequence()
+    {
+        startSequence.Play();
+    }
+
+    public void PlayRestartSequence()
+    {
+        restartSequence.Play();
+    }
+}
